e094ade76ff3f1b900ab862c25f726db0c75278e
Doge-proge <risusbenignitas@gmail.com>
2024-10-05 15:48:37 +0500

    I created a code to manage files !

---
+++ User.py
@@ -0,0 +1,53 @@
+from User_file import FileRepository
+
+def User_int():
+    print("Which function do you want to use ?")
+    print("1. Write lines")
+    print("2. Read lines")
+    print("3. Read")
+    print("4. Write")
+    print("5. Delete")
+    print("6. Copy")
+    print("7. Rename")
+    print("8. Delete a word")
+
+def usage(func: int, o1: FileRepository):
+    match func:
+        case 1:
+            text = input("Enter the text you want to append: ")
+            o1.write_lines(text)
+        case 2:
+            line = int(input("Enter a number of lines you want to read: "))
+            o1.read_lines(line)
+        case 3:
+            ch = int(input("Enter a number of characters u wanna read: "))
+            o1.read(ch)
+        case 4:
+            text = input("Enter a text you want to continue yours with: ")
+            o1.write(text)
+        case 5:
+            o1.delete()
+        case 6:
+            new_name = input("Enter a new file's name: ")
+            o1.copy(new_name)
+        case 7:
+            new_name = input("Enter new name: ")
+            o1.rename(new_name)
+        case 8:
+            element_to_del = input("Enter the element you want to delet from the file: ")
+            o1.delete_word(element_to_del)
+
+name = input("enter the name of the file")
+o1 = FileRepository(name)
+n = 1
+func = 0
+
+while n:
+    User_int()
+    try:
+        func = int(input("Type the function you want to use: "))
+    except ValueError:
+        print("Invalid Value !")
+    except Exception:
+        print("Invalid !")
+    usage(func, o1)
---
+++ User_file.py
@@ -0,0 +1,60 @@
+import os
+import shutil
+
+class FileRepository:
+    def __init__(self, filename) -> None:
+        self.__f_name = filename
+        self.__rline = 1
+        with open(self.__f_name, "w"):
+            pass
+
+    def read_lines(self, line):
+        self.__rline = line
+        with open(self.__f_name, "r") as f:
+            for i in range(0,self.__rline):
+                data = f.readline()
+                print(data)
+    
+    def write_lines(self, text):
+        with open(self.__f_name, "a") as f:
+            if type(text) in (tuple, list, set):
+                for i in text:
+                    f.write(str(i) + "\n")
+            else:
+                f.write(str(text) + "\n")
+
+    def read(self,bytes = 0):
+        with open(self.__f_name, "r") as f:
+            if bytes:
+                print(f.read())
+            else:
+                print(f.read(bytes))
+    
+    def write(self, text):
+        with open(self.__f_name, "a") as f:
+            f.write(str(text))
+        
+    def delete(self):
+        print("The file has been deleted !")
+        print("Quitting the program ...")
+        os.remove(self.__f_name)
+        quit()
+
+    def rename(self, new_name):
+        os.rename(self.__f_name, new_name)
+        self.__f_name = new_name
+
+    def copy(self, new_name):
+        shutil.copy(self.__f_name, new_name)
+
+    def delete_word(self, element_to_del):
+        with open(self.__f_name, "r+") as f:
+            data = f.read().split()
+            index = 0
+            for i in data:
+                if i == element_to_del:
+                    del data[index]
+                    break
+                index += 1
+            for i in data:
+                f.write(i)
